84c6c322774f8f65c646206d79744be0bea43e68
yaroslavsdev <yaroslav.safronov.09@yandex.ru>
2025-11-12 00:41:35 +0300

    Начал писать калькулятор

--- lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/Calculator.kt
+++ lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/Calculator.kt
@@ -4,7 +4,20 @@ package ru.tbank.education.school.lesson1
  * Метод для вычисления простых арифметических операций.
  */
 fun calculate(a: Double, b: Double, operation: OperationType): Double? {
-    TODO()
+    val result = when (operation) {
+        OperationType.ADD -> a + b
+        OperationType.SUBTRACT -> a - b
+        OperationType.MULTIPLY -> a * b
+        OperationType.DIVIDE -> {
+            if (b != 0.0) {
+                a / b
+            } else {
+                null
+            }
+        }
+        else -> null
+    }
+    return result
 }
 
 /**
